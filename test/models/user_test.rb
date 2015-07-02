287269afe601ad4d2eec4ef7ffc3ee57676523ea
Benjamin Kett <kettx001@umn.edu>
2015-07-02 12:58:19 -0500

    completed ch8

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -74,4 +74,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password_confirmation = @user.password
     assert_not @user.valid?
   end
+
+  test "authenticated? shoudl return false for a user with nil digest" do
+    assert_not @user.authenticated? ''
+  end
 end
